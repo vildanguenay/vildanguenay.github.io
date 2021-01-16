32446bdf78c8d44aee40b41008fc6b39857ec2b4
Vildan Günay <vildan-guenay@hotmail.com>
2021-01-16 11:28:10 +0100

    change description

--- src/components/MyServices/MyServices.js
+++ src/components/MyServices/MyServices.js
@@ -4,27 +4,28 @@ function MyServices() {
   return (
     <section className="myServices" id="services">
       <h2 className="sectionTitle sectionTitleServices">
-        My Path to Webdevelopment
+        My Path to Web Development
       </h2>
       <div className="services">
         <div className="service">
-          <h3 className="servicesHeading">STEP 1: Code Factory Vienna</h3>
+          <h3 className="servicesHeading">STEP 1: Education in Vienna</h3>
           <p className="servicesP">
             {/* <strong> */}
             From back-end to front-end programming languages and frameworks I
-            gained first-hand experience of the most popular programs - most of
-            them you can view by clicking the button below.
+            gained first-hand experience of the most popular programs - you can
+            view them by clicking the button below.
             {/* </strong> */}
           </p>
         </div>
         <div className="service">
-          <h3 className="servicesHeading">STEP 2: Webdeveloper @xxxldigital</h3>
+          <h3 className="servicesHeading">
+            STEP 2: Frontend developer @xxxldigital
+          </h3>
           <p>
             {/* <strong> */}
             After successfully completing the "Fullstack Web Development"
-            education at Code Factory Vienna I started to work at the world's
-            second largest furniture retailer xxxldigital, the IT department of
-            XXXLutz.
+            education I started to work at the world's second largest furniture
+            retailer xxxldigital, the IT department of XXXLutz.
             {/* </strong> */}
           </p>
         </div>
